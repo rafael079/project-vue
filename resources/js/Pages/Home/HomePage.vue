754273497889dc5646c626b19493d0d777b773a0
Rafael de Oliveira Silva <ros.rock079@gmail.com>
2024-01-07 18:08:39 -0300

    Instalação do Inertia.js e Dependencias

---
+++ resources/js/Pages/Home/HomePage.vue
@@ -0,0 +1,4 @@
+<template>
+    <div>Home Page</div>
+</template>
+<script setup></script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,30 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
+
+const appTitle =
+    window.document.getElementsByTagName("title")[0]?.innerText || "RoxCorp";
+
+createInertiaApp({
+    title: (title) => (title ? `${title} - ${appTitle}` : appTitle),
+    resolve: (name) => {
+        const pageComponent = resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue", { eager: true })
+        );
+        return pageComponent;
+    },
+    setup({ el, App, props, plugin }) {
+        const app = createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue, Ziggy);
+
+        app.mount(el);
+    },
+    progress: {
+        color: "#e11d48",
+    },
+});
